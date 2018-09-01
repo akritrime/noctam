09790f737cae6ef452cb5d257ed4e8ae61e03de3
akritrime <akritrime@gmail.com>
2018-09-01 22:59:11 +0530

    added option to play audio on click for chromium

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import './index.scss'
 
 
 window.addEventListener("load", () => {
+
+    
     const bg = document.querySelector(".background")
     const frame = document.querySelector(".frame")
     const modal = document.querySelector(".modal")
@@ -9,17 +11,50 @@ window.addEventListener("load", () => {
     const close = document.querySelector(".close")
     const title = document.getElementById("title")
     const door = document.getElementById("door")
+    
 
-    frame.style.visibility = "visible"
-    modal.style.display = "block"
+    const makeThingsVisible = () => {
+        bg.style.opacity = 1
+        frame.style.visibility = "visible"
+        modal.style.display = "block"
+    }
 
+    if (window.chrome) {
+        const click_msg = document.getElementById("click_msg")
+        const startAudio = () => {
+            Array.from(document.querySelectorAll("audio")).forEach(a => {
+                a.play()
+            })
+        }
+        click_msg.style.visibility = "visible"
+        click_msg.classList.add("text-flicker-in-glow")
+
+        window.addEventListener("click", startAudio, {
+            once: true
+        })
+
+        setTimeout(() => {
+            click_msg.classList.add("text-flicker-out-glow")
+            
+            setTimeout(() => {
+                makeThingsVisible()
+            }, 500)
+
+        }, 1000)
+    } else {
+        makeThingsVisible()
+    }
 
     const toggle = () => {
         modal.classList.toggle("show-modal")
         door.play()
     }
 
+    // bg.click()
+
+
     console.log("Vd82dHVLtfMAP0+kOh2CZQp5rsc=")
+    // console.log(lights.play())
     play.addEventListener("click", toggle)
     close.addEventListener("click", toggle)
 
